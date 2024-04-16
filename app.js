53b97323e981dddc63dd28053e3cb1aa729d1028
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-04-16 04:16:56 +0000

    Second commit

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 console.log("hola mundo")
 
 // Esta es mi función que suma dos números
+
 const sum = (a,b) => {
     return a + b
 }
 
-console.log(sum(7,3))
+const euro = {
+    "JPY":156.5,
+    "USD":1.07,
+    "GBP":0.87,
+}
+
+const fromDollarToYen = (quantity) => {
+    return (quantity * euro.USD) * euro.JPY;
+}
+
+const fromEuroToDollar = (quantity) => {
+    return quantity * euro.USD;
+}
+
+const fromYenToPound = (quantity) => {
+    return (quantity / euro.JPY) / euro.GBP;
+}
 
 // Exporta la función para usarla en otros archivos 
-// (similar a la palabra clave "export" cuando se usa webpack)
-module.exports = { sum };
+module.exports = { sum, fromDollarToYen, fromEuroToDollar, fromYenToPound };
--- test.js
+++ test.js
@@ -1,7 +1,28 @@
-const { sum } = require('./app.js')
+const { sum, fromDollarToYen, fromEuroToDollar, fromYenToPound } = require('./app.js')
 
 test("sumar 5 + 5 para dar 10", () => {
     const total = sum(5,5)
 
     expect(total).toBe(10);
+})
+
+test("5 dólares son 837,275 yenes", () => {
+
+    const yenes = fromDollarToYen(5).toFixed(2)
+
+    expect(yenes).toBe("837.28");
+})
+
+test("10 euros son 10,7 dolares", () => {
+   
+    const dolares = fromEuroToDollar(10).toFixed(1)
+
+    expect(dolares).toBe("10.7");
+})
+
+test("2000 yenes son 14.68 libras", () => {
+   
+    const dolares = fromYenToPound(2000).toFixed(2)
+
+    expect(dolares).toBe("14.69");
 })
